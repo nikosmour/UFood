6312257cba4ffba30d12e5e907414f20856fceb7
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2022-04-23 14:47:06 +0300

    new controllers

--- app/Http/Controllers/DailyMealPlanController.php
+++ app/Http/Controllers/DailyMealPlanController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\MealPlan;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
 
-class UserInfoController extends Controller
+class DailyMealPlanController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -14,15 +14,11 @@ class UserInfoController extends Controller
      */
     public function __invoke()
     {
-        $user=Auth::user();
-        $academic=$user->academic;
-        $relations=null;
-        if ($academic)
-            if($academic->cardApplicant)
-                $relations=[['academic','cardApplicant']];
-            else
-                $relations=[['academic']];
-        $models=[$user];
+        $relation1s=['meal'];
+        $models=MealPlan::with($relation1s)->where('date','>=',now()->toDateString() )->get();
+        foreach ($relation1s as $relation1)
+            $relations[]=explode('.',$relation1);
+
         return view('test',compact('models','relations'));
 
     }
