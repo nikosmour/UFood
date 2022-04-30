3c3a9d5f3f9c64ef8a3cbb2ae97ac35dda073bc5
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2022-04-30 12:57:44 +0300

    update  MealMealPlanSeeder

--- database/seeders/MealMealPlanSeeder.php
+++ database/seeders/MealMealPlanSeeder.php
@@ -14,16 +14,22 @@ class MealMealPlanSeeder extends Seeder
      */
     public function run()
     {
+        $last=null;
         if(\App\Models\Meal::all()->count()===0)
             $this->call(MealSeeder::class);
-        if(\App\Models\MealPlan::all()->count()===0)
+        if(\App\Models\MealPlan::all()->count()<=30) {
+            $last=\App\Models\MealPlan::all('id')->last();
             $this->call(MealPlanSeeder::class);
+        }
+        if(is_null($last))
+            $last_id=0;
+        else
+            $last_id=$last->id;
         $meals=\App\Models\Meal::all();
-        $mealPlans=\App\Models\MealPlan::all();
+        $mealPlans=\App\Models\MealPlan::where('id','>',$last_id)->get();
         foreach ( $mealPlans as $mealPlan){
             $meals=$meals->shuffle();
             $mealPlan->meal()->savemany($meals->slice(0,10));
         }
-
     }
 }
