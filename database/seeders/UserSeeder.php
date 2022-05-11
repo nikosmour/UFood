7b1c10194a5ac3fb1c137de5a914c65ba1aaed8f
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2022-05-11 13:38:47 +0300

    fixup! enum traits and interfaces update

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -21,26 +21,25 @@ class UserSeeder extends Seeder
         for( $i=$users_length;$i>0;$i--){
             $user_status=collect(UserStatusEnum::cases())->random();
 
-            if ($user_status->hasAnyAbility([
+            if ($user_status->canAny([
                 UserAbilityEnum::CARD_OWNERSHIP,
                 UserAbilityEnum::COUPON_OWNERSHIP])) {
                 $academic = \App\Models\Academic::factory()->create([
                     'status' => $user_status->value
                 ]);
-                if ($user_status->hasAbility(UserAbilityEnum::CARD_OWNERSHIP))
+                if ($user_status->can(UserAbilityEnum::CARD_OWNERSHIP))
                     \App\Models\CardApplicant::factory()->for($academic)->create();
-                if ($user_status->hasAbility(UserAbilityEnum::COUPON_OWNERSHIP))
+                if ($user_status->can(UserAbilityEnum::COUPON_OWNERSHIP))
                     \App\Models\CouponOwner::factory()->for($academic)->create();
-            }
-            elseif ($user_status->hasAbility(UserAbilityEnum::COUPON_SELL))
-                    \App\Models\CouponStaff::factory()->create();
-            elseif ($user_status->hasAbility(UserAbilityEnum::CARD_APPLICATION_CHECK))
+            } elseif ($user_status->can(UserAbilityEnum::COUPON_SELL))
+                \App\Models\CouponStaff::factory()->create();
+            elseif ($user_status->can(UserAbilityEnum::CARD_APPLICATION_CHECK))
                 \App\Models\CardApplicationStaff::factory()->create([
-                    'status'=>$user_status->value
+                    'status' => $user_status->value
                 ]);
-            elseif ($user_status->hasAbility(UserAbilityEnum::ENTRY_CHECK))
+            elseif ($user_status->can(UserAbilityEnum::ENTRY_CHECK))
                 \App\Models\EntryStaff::factory()->create([
-                    'status'=>$user_status->value
+                    'status' => $user_status->value
                 ]);
         };
         $this->call([
