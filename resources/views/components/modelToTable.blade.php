7e99e81f990c989495f377f51b1ea99898824b5e
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2022-07-18 19:14:10 +0300

    bug in show if array is emty

--- resources/views/components/modelToTable.blade.php
+++ resources/views/components/modelToTable.blade.php
@@ -1,31 +1,33 @@
 <div>
     <table class="table text-center  table-hover table-col-to-row-sm caption-top">
         <caption>{{ __((isset($caption))?$caption:'table') }}</caption>
-        <thead class="thead-dark">
-        <tr>
-            @foreach ($models[0]->getAttributes() as $key=> $value)
-                <th scope="col">{{$key}}</th>
-            @endforeach
-        </tr>
-        </thead>
-        <tbody>
-        @foreach ($models as $model)
+        @if((count($models)!=0))
+            <thead class="thead-dark">
             <tr>
-                @foreach ($model->getAttributes() as $value)
-                    <td>{{$value}}</td>
+                @foreach ($models[0]->getAttributes() as $key=> $value)
+                    <th scope="col">{{$key}}</th>
                 @endforeach
             </tr>
-            @foreach($model->getRelations() as $name=>$relation)
-                @if(!is_null($relation))
-                    <tr>
-                        <td></td>
-                        <td colspan="{{count($model->getAttributes())-1}}">
-                            @include('components.modelToTable',['models'=>(is_countable($relation))?$relation: [$relation],'caption'=>$name ])
-                        </td>
-                    </tr>
-                @endif
+            </thead>
+            <tbody>
+            @foreach ($models as $model)
+                <tr>
+                    @foreach ($model->getAttributes() as $value)
+                        <td>{{$value}}</td>
+                    @endforeach
+                </tr>
+                @foreach($model->getRelations() as $name=>$relation)
+                    @if(!is_null($relation))
+                        <tr>
+                            <td></td>
+                            <td colspan="{{count($model->getAttributes())-1}}">
+                                @include('components.modelToTable',['models'=>(is_countable($relation))?$relation: [$relation],'caption'=>$name ])
+                            </td>
+                        </tr>
+                    @endif
+                @endforeach
             @endforeach
-        @endforeach
-        </tbody>
+            </tbody>
+        @endif
     </table>
 </div>
