3b2d005585950281215f9cb969378af24008b60e
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-04-06 00:32:39 +0300

    update authorization

--- app/Policies/CardApplicationDocumentPolicy.php
+++ app/Policies/CardApplicationDocumentPolicy.php
@@ -2,26 +2,41 @@
 
 namespace App\Policies;
 
+use App\Models\Academic;
 use App\Models\CardApplication;
+use App\Models\CardApplicationDocument;
+use App\Models\CardApplicationStaff;
 use App\Models\User;
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Auth\Access\Response;
+use Illuminate\Support\Facades\Log;
 
 class CardApplicationDocumentPolicy
 {
     use HandlesAuthorization;
 
     /**
+     * Perform pre-authorization checks.
+     */
+    public function before(User $user, string $ability, $document = null, CardApplication $cardApplication): bool|null
+    {
+//        return true;
+//        dump("User $user \n attempting $ability on \n $document other \n $cardApplication");
+        $applicationAbility = match ($ability) {
+            'view', 'viewAny' => 'view',
+            'create', 'update', 'delete' => 'update',
+        };
+        return ($user->can($applicationAbility, $cardApplication)) ? null : false;
+    }
+    /**
      * Determine whether the user can view any models.
      * @param User $user
      * @param CardApplication $cardApplication
      * @return Response|bool
      */
-    public function viewAny(User $user, CardApplication $cardApplication)
+    public function viewAny(User $user, CardApplication $cardApplication): Response|bool
     {
-        if (Auth('cardApplicationStaffs')->user())
-            return true;
-        return $user->academic_id == $cardApplication->academic_id;
+        return true;
     }
 
     /**
@@ -33,9 +48,7 @@ class CardApplicationDocumentPolicy
      */
     public function view(User $user, CardApplication $cardApplication)
     {
-        if (Auth('cardApplicationStaffs')->user())
-            return true;
-        return $user->academic_id == $cardApplication->academic_id;
+        return true;
     }
 
     /**
@@ -44,10 +57,10 @@ class CardApplicationDocumentPolicy
      * @param \App\Models\User $user
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    /*    public function create(User $user)
-        {
-            return isset($user->cardApplicant);
-        }*/
+    public function create(User $user, CardApplication $cardApplication): Response|bool
+    {
+        return true;
+    }
 
     /**
      * Determine whether the user can update the model.
@@ -56,9 +69,9 @@ class CardApplicationDocumentPolicy
      * @param \App\Models\CardApplication $cardApplication
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function update(User $user, CardApplication $cardApplication)
+    public function update(User $user, CardApplicationDocument $cardApplicationDocument, CardApplication $cardApplication)
     {
-        return $user->academic_id == $cardApplication->academic_id;
+        return true;
     }
 
     /**
@@ -68,9 +81,9 @@ class CardApplicationDocumentPolicy
      * @param \App\Models\CardApplication $cardApplication
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function delete(User $user, CardApplication $cardApplication)
+    public function delete(User $user, CardApplicationDocument $cardApplicationDocument, CardApplication $cardApplication): Response|bool
     {
-        //
+        return true;
     }
 
     /**
@@ -80,9 +93,9 @@ class CardApplicationDocumentPolicy
      * @param \App\Models\CardApplication $cardApplication
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function restore(User $user, CardApplication $cardApplication)
+    public function restore(User $user, CardApplication $cardApplication): Response|bool
     {
-        //
+        return false;
     }
 
     /**
@@ -92,9 +105,9 @@ class CardApplicationDocumentPolicy
      * @param \App\Models\CardApplication $cardApplication
      * @return \Illuminate\Auth\Access\Response|bool
      */
-    public function forceDelete(User $user, CardApplication $cardApplication)
+    public function forceDelete(User $user, CardApplication $cardApplication): Response|bool
     {
-        //
+        return false;
     }
 
 
