cd0bacac7d3e37cb863905727410c7478baef2e7
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-04-19 00:51:41 +0300

    update cardChecking
    manage auth on App.vue

--- resources/js/Pages/CardApplicationChecking.vue
+++ resources/js/Pages/CardApplicationChecking.vue
@@ -15,38 +15,26 @@
             </thead>
             <tbody>
             <tr v-for="item in applications" :key="item.id" @click="showSecondTable(item)">
-                <td>{{ item.id }}</td>
+                <td>
+                    <router-link :to="{name:'cardApplicationChecking.application',params:{application:item.id}}"
+                                 class="nav-link router-link-exact-active" replace>{{ item.id }}
+                    </router-link>
+                </td>
             </tr>
             </tbody>
         </table>
-        <div v-if="selectedItem !== null" class='col-auto row'>
-            <CardApplicationShowData v-bind:application="selectedItem"/>
-            <div class="col-auto">
-                <h4>Application Status</h4>
-                <div>
-                    <label for="commentStaff">Enter text:</label>
-                    <input id="commentStaff" v-model="commentChecking" type="text">
-                    <label for="expiration_date">Enter text:</label>
-                    <input id="expiration_date" v-model="expirationDate" type="date">
-
-                </div>
-                <select v-model="selectedItem.status" v-on:change="updateStatus(selectedItem)">
-                    <option disabled value="">Please select one</option>
-                    <option
-                        v-for="status in ['ACCEPTED','REJECTED','INCOMPLETE']"
-                        :key="'CardStatusEnum.'+status" :value="$enums.CardStatusEnum[status]"> {{ status }}
-                    </option>
-                </select>
-                <message v-bind="result"></message>
-            </div>
-        </div>
+        <CardApplicationShowData class="col-auto" v-bind:application="selectedItem"/>
+        <message v-bind="result"></message>
     </div>
 </template>
 
 
 <script>
 
+import CardApplicationShowData from "../Components/CardApplicationShowData.vue";
+
 export default {
+    components: {CardApplicationShowData},
     data() {
         return {
             search: {
@@ -56,9 +44,6 @@ export default {
                 a_m: null,
             },
             selectedItem: null,
-            commentChecking: null,
-            expirationDate: null,
-            currentStatus: null,
             result: {
                 message: '',
                 success: true,
@@ -66,9 +51,15 @@ export default {
                 errors: ['']
             },
             applications: [],
-            category: this.$route.params.category,
         };
-
+    },
+    computed: {
+        category() {
+            return this.$route.params.category;
+        },
+        applicationId() {
+            return this.$route.params.application || this.$route.query.application;
+        },
     },
     methods: {
         broadcasting() {
@@ -112,18 +103,12 @@ export default {
             ).then(function (responseJson) {
                 let applications = responseJson['data'];
                 let success = applications.length > 0;
-                if (success && name === 'application_id') {
-                    vue.selectedItem = applications[0];
-                    vue.currentStatus = applications[0].card_last_update.status;
-                }
                 vue.result.success = success;
                 vue.result.errors = [];
                 if (success) {
                     vue.result.message = "Applications found";
                     return applications;
                 }
-                //else
-                vue.selectedItem = vue.currentStatus = null;
                 vue.result.message = "Request failed: Application don't found";
                 return [];
             }).catch(function (errors) {
@@ -139,51 +124,19 @@ export default {
 
         async startingData() {
             console.log('cardApplicationChecking.startingData')
-            this.applications = await this.getApplications('status', this.category);
+            this.getApplications('status', this.category).then(
+                applications => {
+                    return this.applications = applications;
+                }
+            );
+
+            this.selectedItem = this.applicationId ? (await this.getApplications('application_id', this.applicationId))[0] : null;
+
+
         },
         showSecondTable(item) {
             console.log('showSecondTable');
-            this.getApplications('application_id', item.id);
-        },
-        updateStatus(application) {
-            let params = new FormData();
-            let vue = this;
-            //params.append('_method','PUT')
-            // params.append(`id`, application.id);
-            params.append(`status`, application.status);
-            params.append('card_application_id', application.id)
-            if (this.expirationDate) {
-                params.append('expiration_date', this.expirationDate)
-            }
-            if (this.commentChecking) {
-                params.append('card_application_staff_comment', this.commentChecking)
-            }
-            console.log(params);
-            axios.post(route('cardApplication.checking.store', {'category': application.status}), params
-            ).then(function (responseJson) {
-                let json = responseJson['data'];
-                // application.success = json['success'];
-                //  application.message = json['message'];
-                //application.message='the application is not exist or is ureadable please upload a new one';
-                vue.result.success = json == 1;
-                vue.result.errors = [];
-            }).catch(function (errors) {
-                vue.result.errors = errors.response.data.errors;
-                vue.result.success = false
-            }).finally(() => {
-                if (vue.result.success) {
-                    vue.result.message = "Change from " + vue.currentStatus + ' to ' + application.status;
-                    vue.currentStatus = application.status;
-                    vue.updateApplicationsIds({
-                        'cardApplication_id': application.id,
-                        status: application.status
-                    })
-                    return;
-                }//else
-                application.status = vue.currentStatus;
-                vue.result.message = "Request failed:";
-            });
-
+            // this.getApplications('application_id', item.id);
         },
         updateApplicationsIds(e) {
             let cardApplication_id = e.cardApplication_id;
@@ -199,10 +152,26 @@ export default {
         }
 
     },
-    created() {
+    mounted() {
         this.startingData();
         this.broadcasting();
 
+    },
+    watch: {
+        category(newValue, oldValue) {
+            this.startingData()
+            this.broadcasting();
+            if (typeof Echo !== 'undefined' && typeof oldValue !== 'undefined')
+                Echo.leave(`cardChecking.${oldValue}`);
+        },
+        async applicationId(newValue) {
+            this.selectedItem = newValue ? (await this.getApplications('application_id', newValue))[0] : null;
+        }
+
+    },
+    beforeRouteLeave(from, to) {
+        if (typeof Echo !== 'undefined' && typeof this.category !== 'undefined')
+            Echo.leave(`cardChecking.${this.category}`);
     },
 }
 </script>
--- resources/js/router.js
+++ resources/js/router.js
@@ -6,7 +6,7 @@ const EntryChecking = () => import('./Pages/EntryChecking.vue');
 const Login = () => import('./Pages/Auth/Login.vue');
 const UserProfile = () => import("./Pages/UserProfile.vue");
 const CardApplicationChecking = () => import("./Pages/CardApplicationChecking.vue");
-
+const CardApplicationShowData = () => import("./Components/CardApplicationShowData.vue")
 
 const routes = [
     {
@@ -26,7 +26,10 @@ const routes = [
     },
     {
         path: '/checking/:category', name: 'cardApplication.Checking', component: CardApplicationChecking,
-
+        meta: {requiresAuth: true},
+        children: [
+            {path: 'application/:application', name: 'cardApplicationChecking.application'},
+        ]
     }
 ];
 const router = createRouter({
