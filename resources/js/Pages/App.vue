d463d2aefe0bd9648b124f34f6e1c2477e6a2f95
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-04-19 21:08:52 +0300

    manage auth on App.vue

--- resources/js/Pages/App.vue
+++ resources/js/Pages/App.vue
@@ -1,4 +1,37 @@
 <script>
+import {mapActions, mapGetters} from "vuex";
+
+export default {
+    computed: {
+        ...mapGetters([
+            'isAuthenticated',
+        ]),
+    },
+    methods: {
+        ...mapActions([
+            'getUser'
+        ]),
+    },
+    watch: {
+        isAuthenticated(newValue) {
+            if (!newValue && this.$route.meta.requiresAuth)
+                this.$router.push({
+                    name: 'login',
+                    query: {redirect: this.$route.fullPath},
+                })
+            else if (newValue && this.$route.name === 'login')
+                this.$router.push(this.$route.query.redirect || {name: 'userProfile'});
+        }
+    },
+    mounted() {
+        // setInterval(this.updateAxiosCSRF, 60000);
+        setInterval(() => {
+            if (this.isAuthenticated)
+                this.getUser();
+        }, 300000)
+    },
+
+}
 </script>
 
 <template>
