68ba9e35cb92ed7887a0ea026bfec05aa110690b
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-05-19 23:01:26 +0300

    update StoreCardApplicationCheckingRequest.php

--- app/Http/Requests/StoreCardApplicationCheckingRequest.php
+++ app/Http/Requests/StoreCardApplicationCheckingRequest.php
@@ -3,10 +3,9 @@
 namespace App\Http\Requests;
 
 use App\Enum\CardStatusEnum;
-use App\Models\CardApplication;
-use App\Rules\InArray;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Validation\Rule;
+use Illuminate\Validation\Rules\Enum;
 
 class StoreCardApplicationCheckingRequest extends FormRequest
 {
@@ -28,12 +27,12 @@ class StoreCardApplicationCheckingRequest extends FormRequest
     public function rules()
     {
         return [
-            'status' => ['required', new InArray(CardStatusEnum::values()->toArray())],
+            'status' => ['required', new Enum(CardStatusEnum::class)],
             'card_application_id' => ['required', "exists:card_applications,id"],
             'expiration_date' => ['date',
                 Rule::requiredIf($this->input('status') == CardStatusEnum::ACCEPTED->value),
-                'after_or_equal:' . CardApplication::whereId($this->input('card_application_id'))
-                    ->select('expiration_date')->value('expiration_date')
+                'after_or_equal:' . now()->format('Y-m-d'),
+
             ],
             'card_application_staff_comment' => ['string', Rule::requiredIf($this->input('status') != CardStatusEnum::ACCEPTED->value)]
 
