275843fe063e8973cccd892fc6e12108ea7ad951
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-09-13 17:44:04 +0300

    data seeders   resolve a problem with  factory and __construct in Academic.php

--- app/Models/Academic.php
+++ app/Models/Academic.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
 /**
  * @mixin IdeHelperAcademic
  */
@@ -10,19 +12,28 @@ class Academic extends User
     public $incrementing = false;
     protected $primaryKey = 'academic_id';
 
-    public function __construct()
-    {
-        $this->casts['is_active'] = 'boolean'; // Define the column as boolean
-        parent::__construct();
 
+    /**
+     * Merge new casts with existing casts on the model.
+     *
+     * @param array $casts
+     * @return $this
+     */
 
-    }
-    public function cardApplicant(): \Illuminate\Database\Eloquent\Relations\HasOne
+//    public function __construct()
+//    {
+//        parent::__construct();
+//        $this->casts['is_active'] = 'boolean'; // Define the column as boolean
+//
+//
+//    }
+
+    public function cardApplicant(): HasOne
     {
         return $this->hasOne(CardApplicant::class, 'academic_id');
     }
 
-    public function couponOwner(): \Illuminate\Database\Eloquent\Relations\HasOne
+    public function couponOwner(): HasOne
     {
         return $this->hasOne(CouponOwner::class, 'academic_id');
     }
--- app/Models/User.php
+++ app/Models/User.php
@@ -44,6 +44,7 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
         'status' => UserStatusEnum::class,
+        'is_active' => 'boolean',
     ];
 
     /**
