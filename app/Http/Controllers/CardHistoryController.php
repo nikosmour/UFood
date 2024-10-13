6a9fdcf7016f3a1b5d6890f976cf720c108c1005
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-10-13 18:22:32 +0300

    update card transactions backend

--- app/Http/Controllers/CardHistoryController.php
+++ app/Http/Controllers/CardHistoryController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Enum\UserAbilityEnum;
 use Illuminate\Contracts\Foundation\Application;
 use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\View\View;
@@ -14,7 +15,7 @@ class CardHistoryController extends Controller
     public function __construct()
     {
         $this->middleware('auth:academics,entryStaffs,couponStaffs,cardApplicationStaffs');
-        $this->middleware('ability:' . UserAbilityEnum::CARD_OWNERSHIP->name);;
+        $this->middleware('ability:' . UserAbilityEnum::CARD_OWNERSHIP->name);
     }
 
     /**
@@ -23,12 +24,15 @@ class CardHistoryController extends Controller
      * @param Request $request
      * @return Application|Factory|\Illuminate\Contracts\View\View|View
      */
-    public function __invoke(Request $request): Application|Factory|\Illuminate\Contracts\View\View|View|\Illuminate\Http\JsonResponse
+    public function __invoke(Request $request): Application|Factory|\Illuminate\Contracts\View\View|View|JsonResponse
     {
-        $cardApplicant = Auth::user()->cardApplicant()->with('usageCard')->first();
+
+//        $cardApplicant = Auth::user()->cardApplicant()->with('usageCard')->first();
+        $transactions = Auth::user()->cardApplicant->usageCard()->select('date', 'time', 'period')->simplepaginate(10);
+
         return $request->expectsJson()
-            ? response()->json(["transactions" => $cardApplicant, 'success' => true])
-            : view('cardApplicant.index', compact('cardApplicant'));
+            ? response()->json(["transactions" => $transactions, 'success' => true])
+            : view('cardApplicant.index', compact('transactions'));
 
     }
 }
