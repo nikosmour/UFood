2445d13f603f1bb0d831e5930c905edfedd0aad7
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-11-07 10:29:56 +0200

    give the option to  show in ShowListItem.vue items with zero value

--- resources/js/components/ShowListItem.vue
+++ resources/js/components/ShowListItem.vue
@@ -2,9 +2,9 @@
     <v-list>
         <template v-for="(items, listKey) in listItems" :key="listKey">
             <v-list-item v-for="(value, key) in items" :key="key">
-                <v-row v-if="value" align-content="center">
+                <v-row v-if="showAlwaysItems || value" align-content="center">
                     <v-col class=" d-flex justify-end" cols="6">
-                        <v-list-item-title>{{ $t(listKey + '.' + key.toLowerCase()) }}:</v-list-item-title>
+                        <v-list-item-title>{{ $t(listKey + '.' + key.toLowerCase()) }} :</v-list-item-title>
                     </v-col>
                     <v-col align-self="center" cols="6">
                         <v-list-item-subtitle>{{ value }}</v-list-item-subtitle>
@@ -19,7 +19,8 @@
 export default {
     name: 'showListItem',
     props: {
-        listItems: {}
+        listItems: {},
+        showAlwaysItems: false,
     },
     created() {
         console.log('showListItem created')
