1ccf11ec995a7d734e4b43f095a47bfa68f48785
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-11-13 16:28:12 +0200

    update vite Config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,8 @@ import vue from '@vitejs/plugin-vue'; // Import the Vue plugin
 import vuetify from 'vite-plugin-vuetify';
 
 
-export default defineConfig({
+export default defineConfig(({mode}) => {
+    return {
     plugins: [
         laravel({
             input: ['resources/js/main.js', 'resources/sass/app.scss'],
@@ -14,11 +15,28 @@ export default defineConfig({
             autoImport: true,  // Enable auto-import for Vuetify components
         }),
     ],
-    server: {
-        host: '127.0.3.0', // You can set this to your desired host
-        port: 3000, // You can change this port if needed
+        server: mode === 'development' ? {
+            host: '127.0.3.0',  // Set this to the correct dev host
+            port: 3000, // Port for dev server
         hmr: {
-            host: process.env.VITE_HMR_HOST,
+            host: process.env.VITE_HMR_HOST,  // Set to your HMR host if needed
+        },
+        } : undefined,
+        build: {
+            minify: 'terser',  // Use Terser for minification
+            terserOptions: {
+                compress: {
+                    // Remove all console statements (console.log, console.info, etc.)
+                    drop_console: true,
+
+                    // Remove all debugger statements
+                    drop_debugger: true,
+                },
+                output: {
+                    // Remove all comments from the final output
+                    comments: false,
+                },
         },
     },
+    }
 });
