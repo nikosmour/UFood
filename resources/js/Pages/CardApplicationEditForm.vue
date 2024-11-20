14d9cdd2ee1f83ddff8b93069f7d6652eefc0a32
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-11-20 15:07:54 +0200

    CardApplicationEditForm.vue small initiate of  vuetify update

--- resources/js/Pages/CardApplicationEditForm.vue
+++ resources/js/Pages/CardApplicationEditForm.vue
@@ -1,74 +1,93 @@
 <template>
-    <!-- Vue component template -->
-    <div v-if = "!cardApplication">
-        <loading />
-        <message v-bind = "result"></message>
-    </div>
-    <div v-else>
-        <!-- Display application status and expiration date -->
-        <div class = "row ">
-            <div class = "col-5">
-                <!-- Application form -->
-                <header>
-                    <br />
-                    <h4 class = "text-center">{{ title }}</h4>
-                    <button
-                        v-if = "status === $enums.CardStatusEnum.TEMPORARY_SAVED" :aria-label = "$t('save')"
-                        class = "btn btn-secondary" @click = "changeToSave()"
-                    >
-                        <i aria-hidden = "true" class = "bi bi-save"></i>
-                        <span class = "visually-hidden">{{ $t( "save" ) }}</span>
-                    </button>
-                    <button
-                        v-else-if = "applicationEdit" :aria-label = "$t('edit')" class = "btn btn-secondary"
-                        @click = "changeToEdit()"
-                    >
-                        <i aria-hidden = "true" class = "bi bi-pencil"></i>
-                        <span class = "visually-hidden">{{ $t( "edit" ) }}</span>
-                    </button>
-                </header>
+    <v-card :loading = "isLoading">
+        <!-- Card Application Section -->
+        <v-row v-if = "!isLoading">
+            <v-col cols = "5">
+                <v-card>
+                    <v-card-title>
+                        <h4 class = "text-center">{{ title }}</h4>
+                    </v-card-title>
+                    <v-card-actions class = "justify-space-between">
+                        <v-btn
+                            v-if = "status === $enums.CardStatusEnum.TEMPORARY_SAVED"
+                            :aria-label = "$t('save')"
+                            color = "secondary"
+                            @click = "changeToSave"
+                        >
+                            <v-icon start>mdi-content-save</v-icon>
+                            {{ $t( "save" ) }}
+                        </v-btn>
+                        <v-btn
+                            v-else-if = "applicationEdit"
+                            :aria-label = "$t('edit')"
+                            color = "secondary"
+                            @click = "changeToEdit"
+                        >
+                            <v-icon start>mdi-pencil</v-icon>
+                            {{ $t( "edit" ) }}
+                        </v-btn>
+                    </v-card-actions>
 
-                <!-- Form for editing card application -->
-                <form
-                    v-if = "status === $enums.CardStatusEnum.TEMPORARY_SAVED" id = "card_application_form"
-                    v-on:submit.prevent = ""
-                >
-                    <!-- Show card documents -->
-                    <CardDocumentsShow
-                        ref = "CardDocuments" v-bind:applicationEdit = "true"
-                        v-bind:cardApplication = "cardApplication?.id"
-                        v-on:previewFile = "this.docLink= $event"
+                    <!-- Form for Temporary Saved Status -->
+                    <v-form
+                        v-if = "status === $enums.CardStatusEnum.TEMPORARY_SAVED"
+                        id = "card_application_form"
+                        @submit.prevent = "submit_form"
+                    >
+                        <!-- Card Documents -->
+                        <card-documents-show
+                            ref = "CardDocuments"
+                            :applicationEdit = "true"
+                            :cardApplication = "cardApplication?.id"
+                            @previewFile = "docLink = $event"
+                        />
+                        <!-- Comment Input -->
+                        <v-text-field
+                            v-model = "commentStudent"
+                            label = "{{ $t('comment.enter') }}"
+                            outlined
+                        />
+                        <!-- Submit Button -->
+                        <v-btn color = "primary" type = "submit">
+                            {{ $t( "submit" ) }}
+                        </v-btn>
+                    </v-form>
+
+                    <!-- Read-Only Card Documents -->
+                    <card-documents-show
+                        v-else
+                        :applicationEdit = "false"
+                        :cardApplication = "cardApplication?.id"
+                        @previewFile = "docLink = $event"
                     />
-                    <!-- Comment input field -->
-                    <div>
-                        <label for = "commentStudent">{{ $t( "comment.enter" ) }}</label>
-                        <input id = "commentStudent" v-model = "commentStudent" type = "text">
-                    </div>
-                    <!-- Submit button -->
-                    <button class = "btn btn-primary" type = "submit" @click = "submit_form">
-                        {{ $t( "submit" ) }}
-                    </button>
-                </form>
-                <!-- Show card documents if not in edit mode -->
-                <CardDocumentsShow
-                    v-else :applicationEdit = "false"
-                    v-bind:cardApplication = "cardApplication?.id"
-                    v-on:previewFile = "this.docLink= $event"
-                />
-                <br />
-                <!-- Display result message -->
-                <message v-bind = "result"></message>
-                <br />
-            </div>
-            <!-- Display PDF document -->
-            <object class = "col" height = "500px" type = "application/pdf" v-bind:data = "docLink" width = "100%" />
-        </div>
-    </div>
+                </v-card>
+                <message v-bind = "result" />
+            </v-col>
+
+            <!-- PDF Viewer -->
+            <v-col>
+                <v-sheet class = "pa-4" elevation = "1">
+                    <object
+                        :data = "docLink"
+                        height = "500px"
+                        type = "application/pdf"
+                        width = "100%"
+                    />
+                </v-sheet>
+            </v-col>
+        </v-row>
+    </v-card>
 </template>
 
 <script>
-// Import necessary dependencies
+import { mapGetters } from "vuex";
+import CardApplication from "../models/CardApplication.js";
+import CardDocumentsShow from "../Components/CardDocumentsShow.vue";
+
 export default {
+	components : {
+		CardDocumentsShow,
+	},
 	data() {
 		return {
 			// Initialize data properties
@@ -80,24 +99,14 @@ export default {
 			},
 			docLink :         "", // Initialize docLink
 			cardApplication : null, // Initialize cardApplication
-			lastExpiration :  null,
 			commentStudent :  null, // Initialize commentStudent
 
 		};
 	},
 	computed : {
-		// Computed properties
-		status :          function () {
-			return this.cardApplication
-			       ? this.cardApplication.card_last_update.status
-			       : null;
-		},
-		expiration_date : function () {
-			return this.lastExpiration || ( this.cardApplication
-			                                ? this.cardApplication.expiration_date
-			                                : null );
-		},
-		// Check if application is in edit mode
+		...mapGetters( "auth", {
+			status : "cardStatus",
+		} ),
 		applicationEdit() {
 			return [
 				this.$enums.CardStatusEnum.INCOMPLETE,
@@ -111,15 +120,21 @@ export default {
 			                                 ? "card.edit"
 			                                 : "card.show" );
 		},
+		isLoading() {
+			return !this.cardApplication;
+		},
 	},
 	methods :  {
 		// Method to listen for updates
 		broadcasting() {
 			if ( typeof this.$echo !== "undefined" )
-				this.$echo.private( `cardApplication.${ this.cardApplication.id }` )
+				this.$echo
+				    .private( `cardApplication.${ this.cardApplication.id }` )
 				    .listen( "CardApplicationUpdated", ( e ) => {
-					    this.cardApplication.expiration_date = e[ "expiration_date" ];
-					    this.cardApplication.card_last_update.status = e[ "status" ];
+					    this.cardApplication.expiration_date = new Date( e[ "expiration_date" ] );
+					    this.cardApplication.card_last_update.status = this.$enums.CardStatusEnum.findByValue(
+						    e[ "status" ],
+					    );
 				    } );
 		},
 		async changeToEdit() {
@@ -136,15 +151,13 @@ export default {
 		},
 		// Method to fetch card application data
 		getApplication() {
-			let url = this.route( "cardApplication.index" );
+			const url = this.route( "cardApplication.index" );
 			console.log( "getApplication" );
 			return this.$axios
 			           .get( url )
-			           .then( responseJson => {
-				           let json = responseJson[ "data" ];
-				           this.cardApplication = json[ "cardApplication" ];
-				           this.lastExpiration = json[ "lastExpiration" ];
-
+			           .then( ( responseJson ) => {
+				           const json = responseJson[ "data" ];
+				           this.cardApplication = new CardApplication( json[ "cardApplication" ] );
 				           this.broadcasting();
 			           } )
 			           .catch( errors => {
@@ -164,52 +177,48 @@ export default {
 			return await this.update_form( this.$enums.CardStatusEnum.SUBMITTED );
 		},
 		async update_form( status ) {
-			let url = this.route( "cardApplication.update", this.cardApplication );
-			let params = new FormData();
+			const url = this.route( "cardApplication.update", this.cardApplication );
+			const params = new FormData();
 			this.result.message = ""; //#todo more clever way to show if the value is the same
 			if ( !this.applicationEdit ) {
 				this.result.success = false;
-				this.result.message = this.$t( "application_status_not_allow_submission" );
+				this.result.message = this.$t(
+					"application_status_not_allow_submission",
+				);
 			}
 			params.append( "_method", "PUT" );
-			params.append( "status", status );
+			params.append( "status", status.value );
 			if ( this.commentStudent ) {
 				params.append( "comment", this.commentStudent );
 			}
 			console.log( "start axios to application for submission" );
-			this.$axios
-			    .post( url, params )
-			    .then( responseJson => {
-				    console.log( responseJson );
-				    let json = responseJson[ "data" ];
-				    this.result.success = json[ "success" ];
-				    this.result.message = ( json[ "success" ] )
-				                          ? this.$t( "application" ) + " " + this.$t( "changeFromTo", {
-					    "from1" : `status.${ this.cardApplication.card_last_update.status.replace(
-						    " ", "_" ) }`,
-					    "to1" :   `status.${ status.replace( " ", "_" ) }`,
-				    } )
-				                          : this.$t( json[ "message" ] );
-				    this.result.errors = [];
-			    } )
-			    .catch( errors => {
-				    console.log( errors );
-				    this.result.success = false;
-				    this.result.errors = errors.response.data.errors || errors;
-				    this.result.message = this.$t( "request_failed_status_wont_change" );
-			    } )
-			    .finally( () => {
-				              if ( this.result.success ) {
-					              this.cardApplication.card_last_update.status = status;
-				              }
-
-			              },
-			    );
+			return this.$axios
+			           .post( url, params )
+			           .then( ( responseJson ) => {
+				           const json = responseJson[ "data" ];
+				           this.result.success = json[ "success" ];
+				           this.result.message = json[ "success" ]
+				                                 ? `${ this.$t( "application" ) } ${ this.$t( "changeFromTo", {
+						           from1 : `status.${ this.cardApplication.card_last_update.status.key }`,
+						           to1 :   `status.${ status.key }`,
+					           } ) }`
+				                                 : this.$t( json[ "message" ] );
+				           this.result.errors = [];
+			           } )
+			           .catch( ( errors ) => {
+				           this.result.success = false;
+				           this.result.errors = errors.response.data.errors || errors;
+				           this.result.message = this.$t( "request_failed_status_wont_change" );
+			           } )
+			           .finally( () => {
+				           if ( this.result.success ) {
+					           this.cardApplication.card_last_update.status = status;
+				           }
+			           } );
 		},
 	},
 	created() {
 		this.startingData();
-		document.title = this.title;
 	},
 };
 </script>
