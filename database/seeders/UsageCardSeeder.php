19d03f06c0aa5ed0e2576171cc401676b78551c1
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-12-05 13:16:52 +0200

    fix seeder UsageCardSeeder.php  for error in Duplicate Entry

--- database/seeders/UsageCardSeeder.php
+++ database/seeders/UsageCardSeeder.php
@@ -2,10 +2,17 @@
 
 namespace Database\Seeders;
 
-use Database\Seeders\Classes\ManyToManySeeder;
+use App\Models\CardApplicant;
+use App\Models\EntryStaff;
+use Illuminate\Database\QueryException;
+use Illuminate\Database\Seeder;
 
-class UsageCardSeeder extends ManyToManySeeder
+class UsageCardSeeder extends Seeder
 {
+
+    public function __construct(protected string $createdAtMoreThan = '1900-01-01 12:00:00')
+    {
+    }
     /**
      * Run the database seeds.
      *
@@ -13,9 +20,17 @@ class UsageCardSeeder extends ManyToManySeeder
      */
     public function run()
     {
-        $this->make_connection(
-            \App\Models\CardApplicant::where('created_at', '>', $this->createdAtMoreThan)->cursor(),
-            \App\Models\EntryStaff::all(),
-            \App\Models\UsageCard::class, $this->count);
+        $cardApplicants = CardApplicant::where('created_at', '>', $this->createdAtMoreThan)->cursor();
+        foreach ($cardApplicants as $applicant) {
+            /** type {CardApplicant} $applicant */
+            try {
+                $applicant->usageCard()->create([
+                    'entry_staff_id' => EntryStaff::inRandomOrder()->select(['id'])->first()->id
+                ]);
+            } catch (QueryException  $e) {
+                if ($e->getCode() != 23000)
+                    throw $e;
+            }
+        }
     }
 }
