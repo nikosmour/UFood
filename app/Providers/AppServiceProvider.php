0ee8a6e8d6c902327c2d777e155a5592cdbf1c80
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-12-05 19:42:28 +0200

    install telescope.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        if (!$this->app->environment('production')) {
+            $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
+            $this->app->register(TelescopeServiceProvider::class);
+        }
     }
 
     /**
