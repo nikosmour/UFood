247c9e1b359cc868be1374ad6765c331a073775c
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-12-09 12:38:18 +0200

    introduse config in js

---
+++ resources/js/config/AlterableProperties.js
@@ -0,0 +1,14 @@
+export const alterableProperties = {
+	"CardApplicant" :           [
+		"first_year",
+		"cell_phone",
+	],
+	"Address" :                 [
+		"location",
+		"phone",
+	],
+	"CardApplicationDocument" : [
+		"description",
+		"status",
+	],
+};
---
+++ resources/js/config/index.js
@@ -0,0 +1,5 @@
+import { alterableProperties } from "./AlterableProperties";
+
+export const config = {
+	"alterableProperties" : alterableProperties,
+};
