358d7cc3010356292e24f08bea51024259f142c0
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-12-15 06:59:52 +0200

    fixing usage card Seeder

--- database/seeders/UsageCardSeeder.php
+++ database/seeders/UsageCardSeeder.php
@@ -4,11 +4,15 @@ namespace Database\Seeders;
 
 use App\Models\CardApplicant;
 use App\Models\EntryStaff;
+use App\Models\UsageCard;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\QueryException;
 use Illuminate\Database\Seeder;
 
+
 class UsageCardSeeder extends Seeder
 {
+    use WithoutModelEvents;
 
     public function __construct(protected string $createdAtMoreThan = '1900-01-01 12:00:00')
     {
@@ -20,13 +24,12 @@ class UsageCardSeeder extends Seeder
      */
     public function run()
     {
-        $cardApplicants = CardApplicant::where('created_at', '>', $this->createdAtMoreThan)->cursor();
+        /** @var CardApplicant[] $cardApplicants */
+        $cardApplicants = CardApplicant::withOnly([])->where('created_at', '>', $this->createdAtMoreThan)->cursor();
         foreach ($cardApplicants as $applicant) {
-            /** type {CardApplicant} $applicant */
             try {
-                $applicant->usageCard()->create([
-                    'entry_staff_id' => EntryStaff::inRandomOrder()->select(['id'])->first()->id
-                ]);
+                $staff = EntryStaff::inRandomOrder()->first();
+                UsageCard::factory()->for($staff)->for($applicant)->create();
             } catch (QueryException  $e) {
                 if ($e->getCode() != 23000)
                     throw $e;
