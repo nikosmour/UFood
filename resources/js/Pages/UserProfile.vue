94622f602e47f30ee1922046552a515695188d30
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-12-15 20:37:33 +0200

    Update UserProfile.vue

--- resources/js/Pages/UserProfile.vue
+++ resources/js/Pages/UserProfile.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>{{ $t( "Welcome" ) }}, {{ currentUser?.name }}!</h1>
-        <my-models-to-table :caption = "$t('user.information')" :models = "currentUser? [currentUser]:[]" />
+        <my-models-to-table :caption = "$t('user.information')" :models = "currentUser? [user]:[]" />
     </div>
 </template>
 
@@ -15,6 +15,13 @@ export default {
 		...mapGetters( "auth", [
 			"currentUser",
 		] ),
+		user() {
+			const user = this.currentUser.toObject( false );
+			delete user.abilities;
+			delete user.coupon_owner;
+			user.status = this.$t( "status." + user.status.key );
+			return user;
+		},
 	},
 };
 </script>
