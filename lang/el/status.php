a2d2ca52c46d3d893acb6b00c1ffd3fdbba5706e
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2024-12-18 22:56:03 +0200

    update lang

---
+++ lang/el/status.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'TEMPORARY_SAVED' => 'προσωρινά αποθηκευμένο',
+    'SUBMITTED' => 'υποβλήθηκε',
+    'CHECKING' => 'ελεγχόμενο',
+    'TEMPORARY_CHECKED' => 'προσωρινά ελεγμένο',
+    'ACCEPTED' => 'εγκρίθηκε',
+    'REJECTED' => 'απορρίφθηκε',
+    'INCOMPLETE' => 'ατελής',
+
+];
---
+++ lang/en/status.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'TEMPORARY_SAVED' => 'temporary saved',
+    'SUBMITTED' => 'submitted',
+    'CHECKING' => 'checking',
+    'TEMPORARY_CHECKED' => 'temporary checked',
+    'ACCEPTED' => 'accepted',
+    'REJECTED' => 'rejected',
+    'INCOMPLETE' => 'incomplete',
+
+];
