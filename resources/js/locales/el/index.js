a35116c44ed9a1f14af8041a28ee1c848dffcdd4
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2025-01-07 06:12:31 +0200

    update Lang

---
+++ resources/js/locales/el/index.js
@@ -0,0 +1,9 @@
+import el from "@/locales/el/el";
+import validation from "@/locales/el/validation";
+import $vuetify from "@/locales/el/vuetify";
+
+export default {
+	...el,
+	$vuetify,
+	validation,
+};
