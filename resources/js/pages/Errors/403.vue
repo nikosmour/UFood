c725d727aec42c8370f3297c07291e8974e67047
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2025-01-14 20:23:33 +0200

    restructure errors

--- resources/js/pages/Errors/403.vue
+++ resources/js/pages/Errors/403.vue
@@ -1,34 +1,17 @@
 <template>
-    <v-empty-state
+    <error-page
         :action-text = "$t('errors.navigateAfter')"
-        :aria-live = "liveRegion"
-        :image = "imageUrl"
         :text = "$t('errors.forbiddenAccess.details')"
         :title = "$t('errors.forbiddenAccess.value')"
         headline = "Whoops, 403"
-        @click:action = "navigateToProfile"
-    ></v-empty-state>
+    />
 </template>
 
 <script>
+import ErrorPage from "@pages/Errors/ErrorPage.vue";
+
 export default {
-	props :    {
-		liveRegion : {
-			type :    String,
-			default : "assertive", // Default for aria-live region
-		},
-	},
-	methods :  {
-		navigateToProfile() {
-			this.$router.push( { name : "userProfile" } ); // Use router.push for navigation
-		},
-	},
-	computed : {
-		imageUrl() {
-			return "/img/big_logo_Upatras.png";
-		},
-
-	},
-
+	name :       "404",
+	components : { ErrorPage },
 };
 </script>
---
+++ resources/js/pages/Errors/ErrorPage.vue
@@ -0,0 +1,36 @@
+<!-- resources/js/Pages/NotFound.vue -->
+<template>
+    <v-empty-state
+        :aria-live = "liveRegion"
+        :image = "imageUrl"
+        @click:action = "navigateToProfile"
+    ></v-empty-state>
+</template>
+
+<script>
+
+export default {
+	name :     "ErrorPage",
+	props :    {
+		liveRegion : {
+			type :    String,
+			default : "assertive", // Default for aria-live region
+		},
+	},
+	methods :  {
+		navigateToProfile() {
+			this.$router.push( { name : "startPage" } ); // Use router.push for navigation
+		},
+	},
+	computed : {
+		imageUrl() {
+			return "/img/big_logo_Upatras.png";
+		},
+
+	},
+};
+</script>
+
+<style scoped>
+/* Add any custom styles here */
+</style>
