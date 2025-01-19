41a45920796e29f4f09435d41dbe99f0d3ba4add
Nikolaos Mouratidis <mouratidis_nikolaos@upnet.gr>
2025-01-19 11:44:02 +0200

    update AcademicStartPage.vue

--- resources/js/pages/AcademicStartPage.vue
+++ resources/js/pages/AcademicStartPage.vue
@@ -13,15 +13,8 @@ export default defineComponent( {
 	                                computed : {
 		                                ...mapGetters( "auth", [
 			                                "currentUser",
-		                                ] ),
-	                                },
-	                                methods : {
-		                                ...mapGetters( "auth", [
 			                                "hasAbility",
 		                                ] ),
-	                                },
-	                                created() {
-
 	                                },
                                 } );
 </script>
@@ -29,10 +22,10 @@ export default defineComponent( {
 <template>
     <v-container max-width = "80em">
         <v-row>
-            <v-col cols = "12" lg = "6">
-                <my-free-food-status v-if = "hasAbility($enums.UserAbilityEnum.CARD_OWNERSHIP)" />
+            <v-col cols = "12" lg = "6" v-if = "hasAbility($enums.UserAbilityEnum.CARD_OWNERSHIP)">
+                <my-free-food-status />
             </v-col>
-            <v-col cols = "12" lg = "6">
+            <v-col cols = "12" :lg = "hasAbility($enums.UserAbilityEnum.CARD_OWNERSHIP) ?6 : 12">
                 <my-coupon-owner-balance :coupon-owner = "currentUser.coupon_owner"></my-coupon-owner-balance>
             </v-col>
         </v-row>
